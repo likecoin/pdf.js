6162a64880f9eef1fac4d52b8ca39f688e2d7362
William Chong <me@williamchong.cloud>
2024-01-27 01:18:47 +0800

    Default to odd spread mode in wide screen

--- web/app.js
+++ web/app.js
@@ -1310,6 +1310,11 @@ const PDFViewerApplication = {
           let scrollMode = AppOptions.get("scrollModeOnLoad");
           let spreadMode = AppOptions.get("spreadModeOnLoad");
 
+          if (spreadMode === SpreadMode.UNKNOWN) {
+            spreadMode =
+              innerWidth > innerHeight ? SpreadMode.ODD : SpreadMode.NONE;
+          }
+
           if (stored?.page && viewOnLoad !== ViewOnLoad.INITIAL) {
             hash =
               `page=${stored.page}&zoom=${zoom || stored.zoom},` +
@@ -1323,7 +1328,7 @@ const PDFViewerApplication = {
             if (scrollMode === ScrollMode.UNKNOWN) {
               scrollMode = stored.scrollMode | 0;
             }
-            if (spreadMode === SpreadMode.UNKNOWN) {
+            if (stored.spreadMode) {
               spreadMode = stored.spreadMode | 0;
             }
           }
