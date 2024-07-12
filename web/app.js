07b860b1461903f6f43a6ec949ca85d216fe3ec3
AuroraHuang22 <aurora90376@gmail.com>
2024-07-12 16:51:41 +0800

    🥅 Guard data name undefined

--- web/app.js
+++ web/app.js
@@ -3279,14 +3279,14 @@ function webViewerPostMessage(evt) {
           PDFViewerApplication.open({
             data: atob(data.data),
             originalUrl: data.name,
-            filename: `${data.name}.pdf`,
+            filename: data.name ? `${data.name}.pdf` : "liker-land_ebook.pdf",
           });
           break;
         case "openArrayBufferFile":
           PDFViewerApplication.open({
             data: new Uint8Array(data.data),
             originalUrl: data.name,
-            filename: `${data.name}.pdf`,
+            filename: data.name ? `${data.name}.pdf` : "liker-land_ebook.pdf",
           });
           break;
       }
