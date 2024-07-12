7e375a1646b078594ecb8ebf85488ce8d37ac310
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2024-07-12 16:55:00 +0800

    🎨 Set nftName as the filename (#7)
    * 🎨 Set nftName as the filename
    
    * 🥅 Guard data name undefined

--- web/app.js
+++ web/app.js
@@ -3109,12 +3109,14 @@ function webViewerPostMessage(evt) {
           PDFViewerApplication.open({
             data: atob(data.data),
             originalUrl: data.name,
+            filename: data.name ? `${data.name}.pdf` : "liker-land_ebook.pdf",
           });
           break;
         case "openArrayBufferFile":
           PDFViewerApplication.open({
             data: new Uint8Array(data.data),
             originalUrl: data.name,
+            filename: data.name ? `${data.name}.pdf` : "liker-land_ebook.pdf",
           });
           break;
       }
